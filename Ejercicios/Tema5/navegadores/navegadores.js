fd6bcd5abef0eb6d48d3fc2f7cf38adf54f590c2
pablocidon <pablo.cidbar@gmail.com>
2018-01-25 08:52:45 +0100

    Función para el uso de la base de datos

--- Ejercicios/Tema5/navegadores/navegadores.js
+++ Ejercicios/Tema5/navegadores/navegadores.js
@@ -1,6 +1,7 @@
 var esIndexedDB = false;
 var esWebSQL = false;
-
+var dbWeb;
+//Funciones de navegador
 function idNavegador() {
     var agente = window.navigator.userAgent;
     var navegadores = ['Chrome','Firefox','Safari','Opera','Trident','MSIE','Edge','Gecko','Webkit',''];
@@ -30,13 +31,65 @@ function analizarNavegador() {
             alert("Su navegador no se encuentra entre los anteriores.");
             break;
     }
+}
+//Funciones WebSQL
+function abrirWebSQL() {
+    tamano = 4*1024*1024;
+    dbWeb=openDatabase('PrimeraBaseDatos','1.0','Mi primera base de datos',tamano);//Nombre,versión,descripción,tamaño
+    if(dbWeb){
+        dbWeb.transaction(function (tx) {
+            tx.executeSql("create table if not exists Calculadora(" +
+                "idOp integer primary key autoincrement, " +
+                "operador1 text, " +
+                "operandoDB text," +
+                "operador2 text)");
+        });
+    }
+}
+function insertarWebSQL() {
+    dbWeb.transaction(function (tx){
+        tx.executeSql("insert into Calculadora(operador1,operandoDB,operador2) values (?,?,?)",[op1,opDB,op2]);
+    });
+}
+function vaciarTablaWebSQL() {
+    dbWeb.transaction(function (tx){
+        tx.executeSql("delete from Calculadora where 1");
+    });
+}
+function borrarTablaWebSQL() {
+    dbWeb.transaction(function (tx){
+        tx.executeSql("drop table Calculadora");
+    });
+}
+function borrarRegistroWebSQL() {
+    dbWeb.transaction(function (tx){
+        tx.executeSql("delete from Calculadora where idOp=(?)",[numeroCampo]);
+    });
+}
+function borrarWebSQL(DB){
+    dbWeb.transaction(function (tx) {
+        tx.executeSql("drop database"+DB);
+    });
+}
+function buscarWebSQL(codBuscar) {
+    dbWeb.transaction(function (tx){
+        tx.executeSql("select * from Calculadora where idOP=?",[?],function (tx,rx,erx) {
+            console.log(rx);
+        });
+    });
+}
+
+//Función de uso de base de datos
+function usoBBDDNavegador() {
     if(esIndexedDB && esWebSQL){
-        alert("Su navegador soporta IndexedDB y WebSQL.");
-    }else if(esIndexedDB){
-        alert("Su navegador soporta únicamente IndexedDB.");
+        abrirWebSQL();
+        abrirIndexedDB;
     }else if(esWebSQL){
-        alert("Su navegador soporta únicamente WebSQL.");
+        abrirWebSQL();
+    }else if(esIndexedDB){
+        abrirIndexedDB;
     }else{
         alert("Su navegador no soporta ninguna base de datos.");
     }
+
 }
